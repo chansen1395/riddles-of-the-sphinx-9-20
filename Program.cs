ec6cdfc7f15a91c3c4e9f38cc0e9797493443735
Connor Hansen <chansen13@georgefox.edu>
2021-09-20 15:35:20 -0700

    clean up code.

--- Program.cs
+++ Program.cs
@@ -13,9 +13,7 @@ namespace Sphinx
       Riddle riddleThree = new Riddle("What is full of holes but still holds water?", "sponge");
 
       Score playerScore = new Score(0);
-      // playerScore.addScore(0);
       var array = new int[] {0, 1, 2};
-      // int arrayLength = array.Length;
       var rng = new Random();
       rng.Shuffle(array);
       rng.Shuffle(array);
@@ -27,24 +25,20 @@ namespace Sphinx
         {
         Console.WriteLine(ManyRiddles[j].RiddleAnswer);
         }
-      // for (var arrayIndex = 0; arrayIndex < (array.Length - 1); arrayIndex++)
-      // {
         foreach (int j in array)
         {
-          // foreach (Riddle index in ManyRiddles) {
-            Console.WriteLine("What is the answer to this riddle? " + ManyRiddles[j].RiddleBody);
-            string userAnswer = Console.ReadLine();
-            
-            if (userAnswer.ToLower().Contains(ManyRiddles[j].RiddleAnswer)) 
-            {
-              Console.WriteLine("Correct! The answer was " + ManyRiddles[j].RiddleAnswer);
-              playerScore.addScore(1);
-            }
-            else 
-            {
-              Console.WriteLine("Wrong! Try again");
-            }
-          // }
+          Console.WriteLine("What is the answer to this riddle? " + ManyRiddles[j].RiddleBody);
+          string userAnswer = Console.ReadLine();
+          
+          if (userAnswer.ToLower().Contains(ManyRiddles[j].RiddleAnswer)) 
+          {
+            Console.WriteLine("Correct! The answer was " + ManyRiddles[j].RiddleAnswer);
+            playerScore.addScore(1);
+          }
+          else 
+          {
+            Console.WriteLine("Wrong! Try again");
+          }
         }
       Console.WriteLine("Total Correct Answers: " + playerScore.RiddleScore);
     }
--- Riddles/Riddles.cs
+++ Riddles/Riddles.cs
@@ -30,17 +30,17 @@ namespace Sphinx.Riddles
   }
 
   static class RandomExtensions
-{
+  {
     public static void Shuffle<T> (this Random rng, T[] array)
     {
-        int n = array.Length;
-        while (n > 1) 
-        {
-            int k = rng.Next(n--);
-            T temp = array[n];
-            array[n] = array[k];
-            array[k] = temp;
-        }
+      int n = array.Length;
+      while (n > 1) 
+      {
+        int k = rng.Next(n--);
+        T temp = array[n];
+        array[n] = array[k];
+        array[k] = temp;
+      }
     }
-}
+  }
 }
